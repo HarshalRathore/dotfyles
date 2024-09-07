6875bf8cac9a8d97fbaaa16d5b395c8de7bb092a
Harshal Rathore <harshalrathore2014@gmail.com>
2024-09-07 23:34:21 +0530

    added on-demand zsh command cache rehashing

--- .func.zshrc.sh
+++ .func.zshrc.sh
@@ -41,3 +41,18 @@ z() {
         --bind 'enter:become:echo {2..}'
   ) && cd "$dir"
 }
+
+# On-demand ZSH rehashing based on pacman hook
+# credits https://wiki.archlinux.org/title/Zsh#On-demand_rehash
+zshcache_time="$(date +%s%N)"
+autoload -Uz add-zsh-hook
+rehash_precmd() {
+  if [[ -a /var/cache/zsh/pacman ]]; then
+    local paccache_time="$(date -r /var/cache/zsh/pacman +%s%N)"
+    if (( zshcache_time < paccache_time )); then
+      rehash
+      zshcache_time="$paccache_time"
+    fi
+  fi
+}
+add-zsh-hook -Uz precmd rehash_precmd
